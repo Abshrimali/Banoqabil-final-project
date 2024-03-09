9ed5b7866984c4d9a206658fd82893c5154be517
Abhijeet Shrimali <110634665+Abshrimali@users.noreply.github.com>
2024-03-09 19:08:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// const faq = document.querySelectorAll('.faq-contain');
+
+// faq.forEach(faq => {
+//     faq.addEventListener('click', () => {
+//         faq.classList.toggle('active');
+//     })
+// });
+
+
+// // let ans = document.querySelectorAll(".faq-contain");
+// // answers.forEach((event) => {
+// //     event.addEventListener("click", () => {
+// //         if (event.classList.contains("active")) {
+// //             event.classList.remove("active");
+// //         } else {
+// //             event.classList.add("active");
+// //         }
+// //     });
+// // });
